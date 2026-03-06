854abdb03b3bbbe1218a708980d66d39aa8235ca
Haven Goitom <havengoitom2@gmail.com>
2026-03-06 10:17:44 +0300

    Time: 58 ms (34.08%), Space: 39.7 MB (44%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -3,22 +3,20 @@ class Solution:
         total = 0
 
         #so if two nums divided by k remainder is the same then the two nums difference will give us a number who is divisibel by k
-
-        for i in range(len(nums)):
-            total += nums[i]
-            nums[i] = total
         
         # print(nums)
         hash_map = defaultdict(int)
-        hash_map[0] = 1
+        hash_map[0] = -1
+        total = 0
 
         for i in range(len(nums)):
-            val = nums[i] % k
+            total += nums[i]
+            val = total % k
 
-            if hash_map[val] and (i+2) - hash_map[val] >= 2:
+            if val in hash_map and i - hash_map[val] >= 2:
                 return True
-            elif hash_map[val] == 0:
-                hash_map[val] = i+2
+            elif val not in hash_map:
+                hash_map[val] = i
         
         return False
 
