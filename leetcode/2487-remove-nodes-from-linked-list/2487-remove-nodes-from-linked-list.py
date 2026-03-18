a81f303eac684f6c1962ea0b8db8db5221d2d8d7
Haven Goitom <havengoitom2@gmail.com>
2026-03-18 11:30:47 +0300

    Add solution for Remove Nodes From Linked List

--- leetcode/2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
+++ leetcode/2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
@@ -5,25 +5,42 @@
 #         self.next = next
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        stack = []
-        current = head
-        while current:
-            stack.append(current)
-            current = current.next
-
-        current = stack.pop()
-        maximum = current.val
-        result_list = ListNode(maximum)
+        curr = head
         
-        while stack:
-            current = stack.pop()
-            if current.val < maximum:
-                continue
+        def decreMono(curr):
+            
+            if not curr or not curr.next:
+                return curr
+            
+            next_node = decreMono(curr.next)
+            curr.next = next_node
+
+            if curr.val >= next_node.val:
+                return curr
             else:
-                new_node = ListNode(current.val)
-                new_node.next = result_list
-                result_list = new_node
-                maximum = current.val
+                return next_node
+        
+        return decreMono(curr)
+
+        # stack = []
+        # current = head
+        # while current:
+        #     stack.append(current)
+        #     current = current.next
+
+        # current = stack.pop()
+        # maximum = current.val
+        # result_list = ListNode(maximum)
+        
+        # while stack:
+        #     current = stack.pop()
+        #     if current.val < maximum:
+        #         continue
+        #     else:
+        #         new_node = ListNode(current.val)
+        #         new_node.next = result_list
+        #         result_list = new_node
+        #         maximum = current.val
                 
-        return result_list
+        # return result_list
 
