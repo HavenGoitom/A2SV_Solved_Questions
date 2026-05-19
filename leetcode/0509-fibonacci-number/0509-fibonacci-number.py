9a23484c9b1572241dec9867d3a1bf93536d5a40
Haven Goitom <havengoitom2@gmail.com>
2026-05-19 16:36:57 +0300

    Add solution for Fibonacci Number

--- leetcode/0509-fibonacci-number/0509-fibonacci-number.py
+++ leetcode/0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,8 +1,9 @@
 class Solution:
     def fib(self, n: int) -> int:
+        hashmap = {}
 
-        if n == 1:
-            return 1
-        if n == 0:
-            return 0
-        return self.fib(n-1) + self.fib(n-2)
+        if n == 1 or n == 0:
+            return n
+        if n not in hashmap:
+            hashmap[n] = self.fib(n-1) + self.fib(n-2)
+        return hashmap[n]
